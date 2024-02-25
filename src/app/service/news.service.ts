9f1954d57df3426456c32ad4b97a6131bd0dcd12
Paul Osuigwe {LoopCodes} <103355656+Loopcodes@users.noreply.github.com>
2024-02-25 02:28:49 +0100

    api call update

--- src/app/service/news.service.ts
+++ src/app/service/news.service.ts
@@ -14,7 +14,7 @@ export class NewsService {
     return this.https.get('https://newsapi.org/v2/sources?language=en&apiKey=' + this.api_key)
   }
   getArticlesByid(source : string){
-    return this.https.get('https://newsapi.org/v2/top-headlines?sources=' + source + '&apiKey=' + this.api_key)
+    return this.https.get('https://newsapi.org/v2/top-headlines/sources=' + source + '&apiKey=' + this.api_key)
   }
   initArticles(){
     return this.https.get('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=' + this.api_key)
